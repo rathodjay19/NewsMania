a4e8ffe5520fd93eb27a3038e718d203c05fadda
Jay Rathod <jayrathod3738@gmail.com>
2024-09-09 19:15:13 +0530

    third change buttun

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 render() {
      
-    let { title, description ,imageUrl} = this.props;
+    let { title, description ,imageUrl ,newsUrl} = this.props;
     return (
       <div>
-            <div className="card my-2" style={{width: "18rem"}}>
+            <div className="card my-2 h-100" style={{width: "18rem"}}>
                 <img className="card-img-top" src={imageUrl} />
-                <div className="card-body">
+                <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
                     
-                    <a href="/newsdetails" className="btn btn-sm btn-primary">Click to see more</a>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary mt-auto">Click to see more</a>
                 </div>
             </div>
       </div>
---
+++ src/component/abc.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default abc = () => {
+    const func = async() => {
+        const results = await axios.get("");
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
